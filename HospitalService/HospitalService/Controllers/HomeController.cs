08fb243f90d2ffd1b46204b83b9eda0e61c7ca3d
angelina <angelyusha.u@gmail.com>
2020-02-12 20:48:14 +0200

    login page

--- HospitalService/HospitalService/Controllers/HomeController.cs
+++ HospitalService/HospitalService/Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace HospitalService.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
