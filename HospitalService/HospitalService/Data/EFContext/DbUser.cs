1f9e68dfd1a9aae497efb2c2a99f71c4075ef2dc
angelyusha.u <angelyusha.u@gmail.com>
2020-02-12 17:09:40 +0200

    add folder EFContext

---
+++ HospitalService/HospitalService/Data/EFContext/DbUser.cs
@@ -0,0 +1,15 @@
+﻿using HospitalService.Data.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalService.Data.EFContext
+{
+    public class DbUser : IdentityUser<string>
+    {
+        public ICollection<DbUserRole> UserRoles { get; set; }
+        public virtual UserProfile UserProfile { get; set; }
+    }
+}
---
+++ HospitalService/HospitalService/Data/Models/UserProfile.cs
@@ -0,0 +1,28 @@
+﻿using HospitalService.Data.EFContext;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalService.Data.Models
+{
+    public class UserProfile
+    {
+        [Key, ForeignKey("User")] public string Id { get; set; }
+        [Required] public string FirstName { get; set; }
+        [Required] public string LastName { get; set; }
+
+        /// <summary>
+        /// Фото користувача
+        /// </summary>
+        [Required] public string Image { get; set; }
+
+        /// <summary>
+        /// Дата реєстрації
+        /// </summary>
+        public DateTime RegistrationDate { get; set; }
+        public virtual DbUser User { get; set; }
+    }
+}
