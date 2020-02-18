319281d2b6e76fa620e6766ea27a114b992ed600
angelina <angelyusha.u@gmail.com>
2020-02-18 20:41:52 +0200

    dont show img in newsview but else work

---
+++ HospitalService/HospitalService/Controllers/CommonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HospitalService.Data.EFContext;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HospitalService.Controllers
+{
+    public class CommonController : Controller
+    {
+        private readonly EFContext _context;
+
+        public CommonController(EFContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult News()
+        {
+            return View();
+        }
+    }
+}
