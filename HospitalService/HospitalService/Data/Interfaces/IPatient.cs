f1267da17aaf9c4f6c85f10f12d3b2dd6c8e92d5
angelyusha.u <angelyusha.u@gmail.com>
2020-02-24 17:11:24 +0200

    add patient interface

---
+++ HospitalService/HospitalService/Data/Interfaces/IPatient.cs
@@ -0,0 +1,14 @@
+﻿using HospitalService.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalService.Data.Interfaces
+{
+    public interface IPatient
+    {
+        IEnumerable<PatientProfile> GetPatients { get; set; }
+        PatientProfile Patient(int id);
+    }
+}
---
+++ HospitalService/HospitalService/Data/Repository/PatientRepository.cs
@@ -0,0 +1,26 @@
+﻿using HospitalService.Data.Interfaces;
+using HospitalService.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HospitalService.Data.Repository
+{
+    public class PatientRepository : IPatient
+    {
+        private readonly EFContext.EFContext _context;
+        public PatientRepository(EFContext.EFContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<PatientProfile> GetPatients => _context.PatientProfiles;
+
+        IEnumerable<PatientProfile> IPatient.GetPatients { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public PatientProfile Patient(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
