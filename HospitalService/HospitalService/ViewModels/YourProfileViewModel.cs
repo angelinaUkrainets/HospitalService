94f9e08feeb81b4a89bc702733a703eb5f9724c8
angelyusha.u <angelyusha.u@gmail.com>
2020-03-07 14:01:06 +0200

    work on changedata view

--- HospitalService/HospitalService/ViewModels/YourProfileViewModel.cs
+++ HospitalService/HospitalService/ViewModels/YourProfileViewModel.cs
@@ -8,30 +8,35 @@ namespace HospitalService.ViewModels
 {
     public class YourProfileViewModel
     {
+        [Display(Name = "First Name")]
         [RegularExpression
             (@"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$",
         ErrorMessage = "Please enter a valid name")]
         [Required] public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [RegularExpression
             (@"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$",
         ErrorMessage = "Please enter a valid name")]
         [Required] public string LastName { get; set; }
 
+        [Display(Name = "Email")]
         [RegularExpression
             (@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$",
         ErrorMessage = "Please enter a valid email")]
         [EmailAddress, Required] public string Email { get; set; }
 
+        [Display(Name = "Login")]
         [Required] public string Login { get; set; }
 
+        [Display(Name = "Image")]
+        public string ImagePath { get; set; }
 
-        public string ImageName { get; set; }
-
+        [Display(Name = "Date of birth")]
         [RegularExpression
            (@"^\s*(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})\s*$",
        ErrorMessage = "Please enter a valid time of birth(dd.mm.yyyy)")]
-        [Required] public string TimeOfBirth { get; set; }
+        [Required] public string DateBirth { get; set; }
 
         [Required, DataType(DataType.Password),
             Display(Name = "Password")]
@@ -46,6 +51,7 @@ namespace HospitalService.ViewModels
         ErrorMessage = "Please enter a valid password")]
         public string PasswordConfirm { get; set; }
 
+        [Display(Name = "Type of blood")]
         public string TypeBlood { get; set; }
     }
 }
